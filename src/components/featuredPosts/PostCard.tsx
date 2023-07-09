a0ebf7b290fe42d53f0f3729d9bb61efb9c4c925
jhchoi1182 <jhchoi1182@gmail.com>
2023-07-09 20:13:59 +0900

    refactor: 이미지, md 파일명 영어로 변경

--- src/components/featuredPosts/PostCard.tsx
+++ src/components/featuredPosts/PostCard.tsx
@@ -7,18 +7,10 @@ type PostCardProps = {
 };
 
 export default function PostCard({ post: { title, date, category, path } }: PostCardProps) {
-  const encodedFilename = encodeURIComponent(path);
-
   return (
     <Link href={`/${path}`}>
       <article className="rounded-md h-96 overflow-hidden shadow-md hover:shadow-xl dark:border border-lightDark">
-        <Image
-          className="w-full h-52"
-          src={`/images/posts/${encodedFilename}.webp`}
-          alt={title}
-          width={300}
-          height={208}
-        />
+        <Image className="w-full h-52" src={`/images/posts/${path}.webp`} alt={title} width={300} height={208} />
         <div className="flex flex-col items-center p-4">
           <time className="self-end text-gray-700 dark:text-slate-100">{date.toString()}</time>
           <h3 className="flex items-center text-center h-16 text-lg font-bold">{title}</h3>
