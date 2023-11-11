6956004b42d2c63c058acbaa5e5ace58dd1927e8
jhchoi1182 <jhchoi1182@gmail.com>
2023-11-12 04:01:56 +0900

    [FEAT] 프리로드된 이미지 캐싱

--- src/utils/preloadImage.ts
+++ src/utils/preloadImage.ts
@@ -1,6 +1,13 @@
-export const preloadImage = (images: string[]) => {
-  images.forEach((image) => {
-    const preLoadImg = new Image();
-    preLoadImg.src = image;
-  });
-};
+export const preloadImage = (() => {
+  const cachedImages = new Set();
+
+  return (images: string[]) => {
+    images.forEach((image) => {
+      if (!cachedImages.has(image)) {
+        const preLoadImg = new Image();
+        preLoadImg.src = image;
+        cachedImages.add(image);
+      }
+    });
+  };
+})();
