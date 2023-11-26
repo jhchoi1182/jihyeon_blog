3d1bfa54b572ce080b359866a69a2e96715bc34b
jhchoi1182 <jhchoi1182@gmail.com>
2023-11-27 08:56:05 +0900

    feat: 23.11.26 문제 풀이

--- test.py
+++ test.py
@@ -1,22 +1,31 @@
-n = int(input())
+def merge_sort(array):
+# 1개가 될 때까지 재귀적으로 쪼갬
+    if len(array) <= 1:
+        return array
 
-seller = dict()
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
 
-for i in range(n):
-    book = input()
-    if book not in seller:
-        seller[book] = 1
-    else:
-        seller[book] += 1
+# 왼쪽, 오른쪽 모두 남았을 경우
+    i, j, k = 0, 0, 0
+    while len(left) > i and len(right) > j:
+        if left[i] > right[j]:
+            array[k] = left[i]
+            i += 1
+        else:
+            array[k] = right[j]
+            j += 1
+        k += 1
 
-max_count = 0
-result = ""
+# 왼쪽, 오른쪽 한 쪽을 먼저 다 채워넣었을 경우
+    while len(left) > i:
+        array[k] = left[i]
+        k += 1
+        i += 1
+    while len(right) > j:
+        array[k] = right[j]
+        k += 1
+        j += 1
 
-for (book, count) in seller.items():
-    if max_count < count:
-        max_count = count
-        result = book
-    elif max_count == count:
-        result = sorted([result, book])[0]
-
-print(result)
+    return array
